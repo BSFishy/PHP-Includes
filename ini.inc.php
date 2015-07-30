7f50309f649d7a7ee1b04e42c444f0cd06461317
mttprvst13 <mttprvst13@users.noreply.github.com>
2015-07-30 16:23:07 -0500

    Remove unneeded stuff

--- ini.inc.php
+++ ini.inc.php
@@ -1,7 +1,7 @@
 <?php
 // host
 $host = ($_SERVER['HTTP_HOST'] == 'localhost' ? 'localhost' : 'database host');
-$hostssl = ($_SERVER['HTTP_HOST'] == 'localhost' ? '/localhost' : 'classrmcash.com.nu');
+//$hostssl = ($_SERVER['HTTP_HOST'] == 'localhost' ? '/localhost' : 'classrmcash.com.nu');
 $host2 = ($_SERVER['HTTP_HOST'] == 'localhost' ? 'local directory' : 'directory on online server');
 $wsite = 'localhost';
 $wsitename = 'Websitemane';
@@ -14,15 +14,4 @@ $dbpass = 'user password';
 // brute force login protection
 $ini['bftime'] = 600; 	// seconds to wait until allowed to login again
 $ini['bfattempts'] = 5;	// # of login attempts before blocked
-
-// email
-$admin = 'admin@thefabook.com';
-$returnpath = 'noreply@thefabook.com';
-$from = 'FB Website <noreply@thefabook.com>';
-$bcc = 'mttprvstanddad@gmail.com';
-$mailserver = 'mail.heartofseagrove.com';
-$port = 26;
-$username = 'noreply+thefabook.com';
-$emailpass = '20hos12';
-$hostsite = 'WS';
 ?>
